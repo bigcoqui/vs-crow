d8c9d21bf3c969a5200fbb62cb9e21b7d6fd8ec6
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-17 17:14:05 -0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.graphics.frames.FlxAtlasFrames;
 import openfl.utils.AssetType;
+import openfl.utils.Assets;
 import openfl.utils.Assets as OpenFlAssets;
 import sys.io.File;
 import flash.display.BitmapData;
