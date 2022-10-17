b0b257c2861bb6e089749d86d6d99c3be7f9ae30
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-17 10:31:51 -0300

    Update CachingState.hx

--- source/states/CachingState.hx
+++ source/states/CachingState.hx
@@ -8,7 +8,9 @@ import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
 import lime.app.Application;
+#if !android
 import Discord.DiscordClient;
+#end
 import flixel.FlxSprite;
 import Options;
 import flixel.ui.FlxBar;
